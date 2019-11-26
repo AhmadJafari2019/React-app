3993ebb022658f33600adc2845c2d43c409b30ef
Ahmad <ahmad.hazara@gmx.ch>
2019-11-26 10:58:12 +0100

    Updating the state Immutably with the spread operator and slice method.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
         })
     }
     deletePersonsHandler = (personIndex) => {
-        const persons = this.state.persons;
+        const persons = [...this.state.persons];
+        // const persons = this.state.persons.slice();
         persons.splice(personIndex, 1);
         this.setState({ persons: persons });
     }
