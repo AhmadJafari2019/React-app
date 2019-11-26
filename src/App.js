399a529e472f6deee29365baa1360997b9eac94e
Ahmad <ahmad.hazara@gmx.ch>
2019-11-26 10:34:30 +0100

    List the output of Persons Array and remove the SwitchHandler method

--- src/App.js
+++ src/App.js
@@ -59,10 +59,7 @@ class App extends Component {
         if (this.state.showPerson) {
             persons = (
                 <div>
-                        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "you")}
-                            changed={this.nameChangeHandler}>My hobby is Racing.</Person>
-                        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                    {this.state.persons.map(person => { return <Person name={person.name} age = {person.age}/>})}
                 </div>);
         } 
         return (
